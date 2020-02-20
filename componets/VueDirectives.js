0da17b05a01c7711a9e1226596ff4d9c9ba00790
Hugo Guerrero <hugo.guerrero@ariwonto.com>
2020-02-20 15:41:12 -0500

    modificando y probando los cambios

--- componets/VueDirectives.js
+++ componets/VueDirectives.js
@@ -1,10 +1,12 @@
 Vue.component('vue-directives', {
     template: `<div>
+        <h1 v-text="title"></h1>
         <p v-text="text"></p>
         <a v-bind:href="link.href" v-bind:title="link.title" v-text="link.text"></a>    
     </div>`,
     data() {
         return {
+            title: 'Directivas de Vue.js',
             text: 'Texto de prueba v-text',
             link: {
                 text: 'Vuejs Home',
